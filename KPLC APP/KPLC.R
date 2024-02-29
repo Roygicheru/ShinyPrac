e50b08ed64bd9bf3dc59866b8e73a05f4adeecad
Roygicheru <gicheru.k.roy@gmail.com>
2024-02-29 21:03:44 +0300

    done

--- KPLC APP/KPLC.R
+++ KPLC APP/KPLC.R
@@ -1,123 +1,138 @@
-# Load the shiny and openxlsx packages
 library(shiny)
 library(openxlsx)
+library(shinyjs)
 
 # Define the UI
 ui <- fluidPage(
-  # A title for the app
-  titlePanel("Employee Training App"),
+  useShinyjs(),
+  tags$head(
+    tags$style(HTML("
+      #header {
+        color: white;
+        background-color: #00008B;
+        padding: 20px;
+        text-align: center;
+        font-size: 24px;
+      }
+      #header img {
+        float: left;
+      }
+      .button-row {
+        display: flex;
+        justify-content: space-between;
+      }
+    "))
+  ),
+  tags$header(
+    id = "header",
+    img(src = "https://th.bing.com/th/id/OIP.5m8zEp8lfuvVsi8kJPwU0QHaF5?w=213&h=180&c=7&r=0&o=5&dpr=1.3&pid=1.7", height = "50px"), # Replace with your image path
+    "Training Registration Page"
+  ),
+  titlePanel(" ", windowTitle = "Training Registration"),
   
-  # A main panel with two columns
-  mainPanel(
-    # A column for internal employee training
-    column(6,
-           # A button to toggle the input fields
-           actionButton("internal", "Internal Employee Training"),
-           # A conditional panel that shows the input fields when the button is clicked
-           conditionalPanel(
-             condition = "input.internal > 0",
-             # An input field for the first name of the participant
-             textInput("fname", "First Name"),
-             # An input field for the second name of the participant
-             textInput("sname", "Second Name"),
-             # An input field for the surname of the participant
-             textInput("lname", "Surname"),
-             # An input field for the staff number of the participant
-             numericInput("staff", "Staff Number", value = NULL),
-             # An input field for the designation of the participant
-             textInput("desig", "Designation"),
-             # An input field for the mobile number of the participant
-             numericInput("mobile", "Mobile Number", value = NULL),
-             # An input field for the station/region of the participant
-             textInput("station", "Station/Region"),
-             # An input field for the supervisor of the participant
-             textInput("super", "Supervisor"),
-             # A submit button that saves the input to an excel sheet
-             actionButton("submit1", "Submit")
-           )
-    ),
-    
-    # A column for corporate employee training
-    column(6,
-           # A button to toggle the input fields
-           actionButton("corporate", "Corporate Employee Training"),
-           # A conditional panel that shows the input fields when the button is clicked
-           conditionalPanel(
-             condition = "input.corporate > 0",
-             # An input field for the name of the client
-             textInput("name2", "Name"),
-             # An input field for the ID number of the client
-             numericInput("id2", "ID Number", value = NULL),
-             # A submit button that saves the input to an excel sheet
-             actionButton("submit2", "Submit")
-           )
-    )
+  tags$head(
+    tags$style(HTML("
+      #internal:hover, #corporate:hover {
+        background-color: #00008B;
+        color: white;
+      }
+    "))
+  ),
+  
+  div(class = "button-row",
+      column(6,
+             actionButton("internal", "Internal Employee Training"),
+             br(), br(), # Line break after the internal button
+             uiOutput("internalUI")
+      ),
+      column(6,
+             actionButton("corporate", "Corporate Employee Training"),
+             br(), br(), # Line break after the corporate button
+             uiOutput("corporateUI")
+      )
   )
 )
 
 # Define the server
 server <- function(input, output, session) {
-  # A reactive value to store the data
-  data <- reactiveValues()
+  activeMenu <- reactiveVal(NULL)
   
-  # A function to append a row to an excel sheet
-  append_row <- function(file, row) {
-    # If the file does not exist, create it with column names
-    if (!file.exists(file)) {
-      write.xlsx(c("First Name", "Second Name", "Surname", "Staff Number", "Designation", "Mobile Number", "Station/Region", "Supervisor"), file, colNames = FALSE)
+  output$internalUI <- renderUI({
+    if(!is.null(activeMenu()) && activeMenu() == "internal") {
+      fluidRow(
+        textInput("fname", "First Name"),
+        textInput("sname", "Second Name"),
+        textInput("lname", "Surname"),
+        numericInput("staff", "Staff Number", value = NULL),
+        textInput("desig", "Designation"),
+        numericInput("mobile", "Mobile Number", value = NULL),
+        textInput("station", "Station/Region"),
+        textInput("super", "Supervisor"),
+        actionButton("submit1", "Submit", class = "btn-primary")
+      )
     }
-    # Read the existing data from the file
-    old_data <- read.xlsx(file)
-    # Append the new row to the old data
-    new_data <- rbind(old_data, row)
-    # Write the new data to the file
-    write.xlsx(new_data, file, colNames = FALSE)
-  }
+  })
   
+  output$corporateUI <- renderUI({
+    if(!is.null(activeMenu()) && activeMenu() == "corporate") {
+      fluidRow(
+        textInput("participant", "Name of Participant"),
+        numericInput("nationalID", "National ID", value = NULL),
+        textInput("company", "Company"),
+        numericInput("mobileNo", "Mobile No.", value = NULL),
+        actionButton("submit2", "Submit", class = "btn-primary")
+      )
+    }
+  })
   
-  # Observe the submit button for internal employee training
+  observeEvent(input$internal, {
+    activeMenu("internal")
+  })
+  
+  observeEvent(input$corporate, {
+    activeMenu("corporate")
+  })
+  
   observeEvent(input$submit1, {
-    # Get the input values
-    fname <- input$fname
-    sname <- input$sname
-    lname <- input$lname
-    staff <- input$staff
-    desig <- input$desig
-    mobile <- input$mobile
-    station <- input$station
-    super <- input$super
-    # Validate the input values
-    if (is.null(fname) || is.null(sname) || is.null(lname) || is.null(staff) || is.null(desig) || is.null(mobile) || is.null(station) || is.null(super)) {
-      # Show an error message
-      showNotification("Please enter all the required fields", type = "error")
-    } else {
-      # Save the input values to a reactive value
-      data$internal <- c(fname, sname, lname, staff, desig, mobile, station, super)
-      # Append the input values to the excel sheet for internal employee training
-      append_row("internal.xlsx", data$internal)
-      # Show a success message
-      showNotification("Data saved successfully", type = "message")
-    }
+    req(input$fname, input$sname, input$lname, input$staff, input$desig, input$mobile, input$station, input$super)
+    data <- data.frame(
+      First_Name = input$fname,
+      Second_Name = input$sname,
+      Surname = input$lname,
+      Staff_Number = input$staff,
+      Designation = input$desig,
+      Mobile_Number = input$mobile,
+      Station_Region = input$station,
+      Supervisor = input$super
+    )
+    append_row("internal.xlsx", data)
+    showNotification("Internal employee data saved successfully", type = "message")
   })
   
-  # Observe the submit button for corporate employee training
   observeEvent(input$submit2, {
-    # Get the input values
-    name <- input$name2
-    id <- input$id2
-    # Validate the input values
-    if (is.null(name) || is.null(id)) {
-      # Show an error message
-      showNotification("Please enter both name and ID number", type = "error")
-    } else {
-      # Save the input values to a reactive value
-      data$corporate <- c(name, id)
-      # Append the input values to the excel sheet for corporate employee training
-      append_row("corporate.xlsx", data$corporate)
-      # Show a success message
-      showNotification("Data saved successfully", type = "message")
-    }
+    req(input$participant, input$nationalID, input$company, input$mobileNo)
+    data <- data.frame(
+      Name_of_Participant = input$participant,
+      National_ID = input$nationalID,
+      Company = input$company,
+      Mobile_No = input$mobileNo
+    )
+    append_row("corporate.xlsx", data)
+    showNotification("Corporate client data saved successfully", type = "message")
   })
+  
+  append_row <- function(file, row) {
+    if (!file.exists(file)) {
+      wb <- createWorkbook()
+      addWorksheet(wb, "Sheet1")
+      writeData(wb, "Sheet1", row, startRow = 1, colNames = TRUE)
+      saveWorkbook(wb, file, overwrite = TRUE)
+    } else {
+      wb <- loadWorkbook(file)
+      writeData(wb, "Sheet1", row, startRow = getSheetDims(wb, "Sheet1")[1] + 1, colNames = FALSE)
+      saveWorkbook(wb, file, overwrite = TRUE)
+    }
+  }
 }
 
 # Run the app
